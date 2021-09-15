4320af70d78bb3099bb50ed43afe07a5d70298a5
Jonathan Eshom <lightman314@gmail.com>
2021-09-14 21:09:24 -0400

    Fixed a crash when curios isn't loaded.

--- src/main/java/io/github/lightman314/lightmanscurrency/proxy/ClientProxy.java
+++ src/main/java/io/github/lightman314/lightmanscurrency/proxy/ClientProxy.java
@@ -2,7 +2,6 @@ package io.github.lightman314.lightmanscurrency.proxy;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import io.github.lightman314.lightmanscurrency.BlockItemSet;
 import io.github.lightman314.lightmanscurrency.LightmansCurrency;
@@ -12,7 +11,6 @@ import io.github.lightman314.lightmanscurrency.client.gui.screen.TerminalScreen;
 import io.github.lightman314.lightmanscurrency.client.gui.screen.inventory.*;
 import io.github.lightman314.lightmanscurrency.client.model.ModelWallet;
 import io.github.lightman314.lightmanscurrency.client.renderer.blockentity.*;
-import io.github.lightman314.lightmanscurrency.client.renderer.entity.layers.WalletLayer;
 import io.github.lightman314.lightmanscurrency.common.universal_traders.IUniversalDataDeserializer;
 import io.github.lightman314.lightmanscurrency.common.universal_traders.data.UniversalTraderData;
 import io.github.lightman314.lightmanscurrency.core.ModBlocks;
@@ -21,8 +19,6 @@ import io.github.lightman314.lightmanscurrency.integration.Curios;
 import io.github.lightman314.lightmanscurrency.core.ModBlockEntities;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.model.PlayerModel;
-import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
@@ -32,12 +28,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.Constants;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 import net.minecraftforge.fmlclient.registry.ClientRegistry;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 
 public class ClientProxy extends CommonProxy{
 	
@@ -81,12 +72,12 @@ public class ClientProxy extends CommonProxy{
     	{
     		Curios.RegisterCuriosRenderers();
     	}
-    	else //Add wallet layer if curios is not loaded.
+    	/*else //Add wallet layer if curios is not loaded.
     	{
 			Map<String, EntityRenderer<? extends Player>> skinMap = Minecraft.getInstance().getEntityRenderDispatcher().getSkinMap();
 	    	this.addWalletLayer((PlayerRenderer)skinMap.get("default"));
 	    	this.addWalletLayer((PlayerRenderer)skinMap.get("slim"));
-    	}
+    	}*/
     	
 	}
 	
@@ -101,14 +92,14 @@ public class ClientProxy extends CommonProxy{
 		blockItemSet.getAll().forEach(blockItemPair -> ItemBlockRenderTypes.setRenderLayer(blockItemPair.block, type));
 	}
 	
-	private void addWalletLayer(PlayerRenderer renderer)
+	/*private void addWalletLayer(PlayerRenderer renderer)
 	{
 		List<RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>>> layers = ObfuscationReflectionHelper.getPrivateValue(LivingEntityRenderer.class, renderer, "field_177097_h");
 		if(layers != null)
 		{
 			layers.add(new WalletLayer<AbstractClientPlayer,PlayerModel<AbstractClientPlayer>>(renderer, new ModelWallet<AbstractClientPlayer>(Minecraft.getInstance().getEntityModels().bakeLayer(ModLayerDefinitions.WALLET))));
 		}
-	}
+	}*/
 	
 	@Override
 	public void updateTraders(CompoundTag compound)
