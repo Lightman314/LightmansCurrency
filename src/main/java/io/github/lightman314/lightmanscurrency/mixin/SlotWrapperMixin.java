13ce37c897fcbfde89f9aa90ad5daafeb4cca646
Lightman <lightman314@gmail.com>
2024-04-13 22:12:46 -0400

    2.1.2.3 Patch B
    -Reworked slot accessors to match 1.19.2 changes. Now uses access transformers to make Slot#x/y non-final.

--- src/main/java/io/github/lightman314/lightmanscurrency/mixin/SlotWrapperMixin.java
+++ src/main/java/io/github/lightman314/lightmanscurrency/mixin/SlotWrapperMixin.java
@@ -3,23 +3,28 @@ package io.github.lightman314.lightmanscurrency.mixin;
 import io.github.lightman314.lightmanscurrency.LCConfig;
 import io.github.lightman314.lightmanscurrency.client.util.ScreenPosition;
 import io.github.lightman314.lightmanscurrency.common.menus.slots.WalletSlot;
+import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen.SlotWrapper;
 import net.minecraft.world.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen$SlotWrapper")
+@Mixin(SlotWrapper.class)
 public class SlotWrapperMixin {
+
+    @Unique
+    private SlotWrapper self() { return (SlotWrapper)(Object)this; }
 
     @Inject(at = @At("TAIL"), method = "<init>")
     public void init(Slot target, int index, int x, int y, CallbackInfo callbackInfo)
     {
-        if(target instanceof WalletSlot && this instanceof SlotAccessor accessor)
+        if(target instanceof WalletSlot)
         {
             ScreenPosition pos = LCConfig.CLIENT.walletSlotCreative.get();
-            accessor.setX(pos.x + 1);
-            accessor.setY(pos.y + 1);
+            self().x = pos.x + 1;
+            self().y = pos.y + 1;
         }
     }
 
